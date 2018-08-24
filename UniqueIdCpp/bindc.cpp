c75c5a547a54490f08d61309f2e3fbfdf3226815
Andre B. Derraik <andre@tecgraf.puc-rio.br>
2018-08-24 17:03:23 -0300

    Adding Bind for C.

---
+++ UniqueIdCpp/bindc.cpp
@@ -0,0 +1,34 @@
+﻿#include "UniqueId.h"
+#include "unique_id.h"
+
+void* unique_id_c(const char *label)
+{
+	const auto id = new unique_id(label);
+	return id;
+}
+
+void unique_id_release_c(const void* id)
+{
+	if (id == nullptr) return;
+
+	const auto uid = (unique_id*)id;
+	delete uid;
+}
+
+unsigned int unique_id_get(const void *id)
+{
+	if (id == nullptr) return 0;
+
+	const auto uid = (unique_id*)id;
+	return uid->get_id();
+}
+
+#ifdef USE_64
+unsigned long long unique_id_get64(const void *id)
+{
+	if (id == nullptr) return 0;
+
+	const auto uid = (unique_id*)id;
+	return uid->get_id64();
+}
+#endif
---
+++ UniqueIdCpp/unique_id.h
@@ -0,0 +1,12 @@
+﻿#ifndef UNIQUE_ID_BIND_H
+#define UNIQUE_ID_BIND_H
+extern "C" {
+	void* unique_id_c(const char *label);
+	void unique_id_release_c(const void *id);
+	unsigned int unique_id_get(const void *id);
+#ifdef USE_64
+	unsigned long long unique_id_get64(const void *id);
+#endif
+}
+
+#endif
